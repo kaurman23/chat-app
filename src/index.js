4c977a12662648b31d42931d7e73a9b2fd6e9bfb
Manpreet Kaur <kaurman2305@gmail.com>
2020-12-16 18:32:16 +0530

    Send message when user joins or leaves

--- src/index.js
+++ src/index.js
@@ -18,9 +18,14 @@ io.on('connection',(socket) => {
     console.log("New Connection")
 
     socket.emit('message','Welcome')
+    socket.broadcast.emit('message','A new user has joined') //broadcast emits to everyone except himself
 
     socket.on('sendMessage',(message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect',() => {
+        io.emit('message','A user has left!')
     })
 
 })  //event, callback function
